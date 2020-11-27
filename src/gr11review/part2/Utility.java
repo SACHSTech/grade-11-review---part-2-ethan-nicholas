9ed8caa3ed5daa3f8b1ec808ce737abf8a860bdb
Nicholas Lee <nicholas.lee21@ycdsbk12.ca>
2020-11-27 14:34:53 +0000

    Finished Method 1

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,5 +1,25 @@
 package gr11review.part2;
 
 public class Utility {
+  public static String zipZap(String str){
+
+    int intLen;
+    String strNewWord = " ";
     
+    // Added two spaces to the end of the string which adds two extra indices
+    str = str + "  ";
+    intLen = str.length();
+      
+    for(int intCount = 0; intCount < intLen; intCount = intCount + 1){
+      if(str.charAt(intCount) == 'z' && str.charAt(intCount + 2) == 'p'){
+        strNewWord = strNewWord + "z";
+        intCount = intCount + 1;
+      }else{
+        strNewWord = strNewWord + str.charAt(intCount);
+      }
+    }
+
+    return strNewWord;
+
+  }
 }
