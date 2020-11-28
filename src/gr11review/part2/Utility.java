7214cfe46b654e1a85e753055d056af1da6d9f47
Ethan Lai <ethan.lai21@ycdsbk12.ca>
2020-11-28 20:12:52 +0000

    Added Comments to Method 2

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -5,25 +5,34 @@ import java.io.FileReader;
 
 public class Utility {
   public static int Sum(String strString){
-
+    
+    // Variables
     int intLoop;
     int intLoop2;
     int intResult;
     char chrCharacters;
     String strNumbers;
 
+    // Initializing Variables
     strNumbers = "";
     intResult = 0;
 
+    // For loop that allows the program to count how many characters are in the string
     for(intLoop = 0; intLoop < strString.length(); intLoop++){
       chrCharacters = strString.charAt(intLoop);
+
+      // If the next character the program counts is a number, adds the number character to a string
       if (Character.isDigit(chrCharacters)) {
         strNumbers = strNumbers + chrCharacters;
+
+      // If the next character the program counts is not a number, adds the string stored with numbers into intResult. Then the program resets the string.
       } else if (!strNumbers.equals("")) {
         intResult = intResult + Integer.parseInt(strNumbers);
         strNumbers = "";
       }
     }
+    
+    // If strNumbers still has numbers stored in it by the end of the string, it adds the remaining numbers into intResult.
     if(!strNumbers.equals("")){
       intResult = intResult + Integer.parseInt(strNumbers);
     }
