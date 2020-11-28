54600e44efcf4441931e2b19e975495872a44e0a
Ethan Lai <ethan.lai21@ycdsbk12.ca>
2020-11-28 03:28:23 +0000

    Edit 2

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,6 +1,15 @@
+// Given a string, return the sum of the numbers appearing in the string, ignoring all other characters. A number is a series of 1 or more digit chars in a row. (Note: Character.isDigit(char) tests if a char is one of the chars '0', '1', .. '9'. Integer.parseInt(string) converts a string to an int.)
+
 package gr11review.part2;
+import java.io.FileReader;
 
 public class Utility {
-    
+  public static int Sum(String strString){
+
+    int intLoop;
+
+    for(intLoop = 0; intLoop < strString.length(); intLoop++){
+      
+    }
 }
 
