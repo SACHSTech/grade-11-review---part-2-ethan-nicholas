80769bc4fdee97fec52b6f4092ef8e1e8ccce773
Nicholas Lee <nicholas.lee21@ycdsbk12.ca>
2020-11-28 21:32:42 +0000

    Finished Array 1

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -6,7 +6,10 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException{
 
-    System.out.println(Utility.longestWord("src/gr11review/part2/words.txt"));
+    //System.out.println(Utility.longestWord("src/gr11review/part2/words.txt"));
+
+    int[] num = {2,10,3,4,20,5};
+    System.out.println(Utility.tenRun(num));
   
   }
 }
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -51,4 +51,30 @@ public class Utility {
     return strLongestWord;
   }
 
+  public static int[] tenRun(int[] nums){
+
+    int intLen = nums.length;
+
+    for(int intCount = 0; intCount < intLen - 1; intCount = intCount + 1){
+      
+      // if the number is a multiple of 10, then it goes to the next if statement
+      if(nums[intCount] % 10 == 0){
+        // if the next number is not a mutiple of 10
+        // then the number becomes the previous number which is a multiple of 10
+        if(nums[intCount + 1] % 10 != 0){
+          nums[intCount + 1] = nums[intCount];
+        }
+      }
+      
+    }
+
+    // displays the array
+    for(int intCount2 = 0; intCount2 < intLen; intCount2 = intCount2 + 1){
+      System.out.println(nums[intCount2]);
+    }
+
+    return nums;
+
+  }
+
 }
