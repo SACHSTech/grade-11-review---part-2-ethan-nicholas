76c2f6788cf3afc577b3a6b99840d4da3646143a
Ethan Lai <ethan.lai21@ycdsbk12.ca>
2020-11-29 19:30:25 +0000

    Edit 18

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -76,4 +76,9 @@ public class Utility {
     TextFile.close();
     return strResultingWord;
   }
+
+// We'll say that an element in an array is "alone" if there are values before and after it, and those values are different from it. Return a version of the given array where every instance of the given value which is alone is replaced by whichever value to its left or right is larger. public int[] notAlone(int[] nums, int val)
+  public static int[] notAlone(int[] nums, int val){
+    
+  }
 }
