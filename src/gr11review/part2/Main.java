6499922357bfac13833d36d21ad6b28a79a77dfe
Ethan Lai <ethan.lai21@ycdsbk12.ca>
2020-11-29 01:24:23 +0000

    Edit 12

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -8,14 +8,31 @@ public class Main {
 
   public static void main(String[] args) throws IOException {
     BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-    
+
+    // Variables
+    String strSelect;
     String str;
     int intSum;
-    System.out.println("Methods 2");
-    System.out.print("Give me a string: ");
-    str = keyboard.readLine();
-    intSum = Utility.sumNumbers(str);
-    System.out.println("This is the sum: " + intSum);
 
+    System.out.println("Select the program to run: ");
+    System.out.println("Options: Methods 2, ");
+    strSelect = keyboard.readLine();
+
+    switch (strSelect){
+      case "Methods 2" :
+        System.out.print("Give me a string: ");
+        str = keyboard.readLine();
+        intSum = Utility.sumNumbers(str);
+        System.out.println("This is the sum: " + intSum);
+        break;
+      case "File IO 2" :
+        System.out.print("Give me a string: ");
+        str = keyboard.readLine();
+        intSum = Utility.sumNumbers(str);
+        System.out.println("This is the sum: " + intSum);
+      default :
+        System.out.println("That is not an option");
+        break;
+    }
   }
 }
