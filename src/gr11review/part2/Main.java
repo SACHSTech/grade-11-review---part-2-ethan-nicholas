7394edd3ed7d4eb7e4336ff18294439e65be4c82
NicholasLee03 <36207675+NicholasLee03@users.noreply.github.com>
2020-11-30 07:37:06 -0500

    Update Main.java
    Code was deleted from merges

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -8,6 +8,27 @@ import gr11review.part2.Utility.*;
 public class Main {
 
   public static void main(String[] args) throws IOException {
+    
+     // Method 1 - Nicholas Lee
+    System.out.println(Utility.zipZap("zipXzap"));
+
+    // File IO 1 - Nicholas Lee
+    System.out.println(Utility.longestWord("src/gr11review/part2/words.txt"));
+
+    // Array 1 - Nicholas Lee
+    int[] num = {10, 1, 9, 20};
+    Utility.tenRun(num);
+
+    // Array 3 - Nicholas Lee
+    int[] outer = {1,2,4,6};
+    int[] inner = {2,4};
+    System.out.println(Utility.linearIn(outer, inner));
+
+    // Array 5 - Nicholas Lee
+    int i = 3;
+    int j = 4;
+    Utility.pascalTri(i, j);
+    
     BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
     // Scanner to allow user to input values into an array
